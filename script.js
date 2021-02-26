c55a0ee439db48bbd52b71732d45b31c420f6696
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2021-02-27 00:55:49 +1030

    updated

--- script.js
+++ script.js
@@ -21,10 +21,33 @@ document.onkeydown = function(e) {
 
 function password() {
     var keyentered = prompt("Enter password to continue")
-    if (keyentered == 33) {
-       alert("Correct")
+    var x = document.getElementById("toggle-blocker");
+    if (keyentered == GlDkju) {
+       x.style.display = "none";
     }
     else {
+        x.style.display = "block";
         window.location.replace("wrong.html");
     }
 }
+
+function searchpersonslist() {
+   var input, filter, ul, li, a, i, txtValue;
+   input = document.getElementById('search-persons-list');
+   filter = input.value.toUpperCase();
+   ul = document.getElementById("persons-list");
+   li = ul.getElementsByTagName('li');
+ 
+   for (i = 0; i < li.length; i++) {
+     a = li[i].getElementsByTagName("a")[0];
+     txtValue = a.textContent || a.innerText;
+     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+       li[i].style.display = "";
+     } else {
+       li[i].style.display = "none";
+     }
+   }
+ }
+
+var inputvar = "hi";
+document.getElementById("passwordchecker").value = test;
